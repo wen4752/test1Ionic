9d134012861d0cd414d13a593d7820f4f50960f2
mygit <wenaudi74@gmail.com>
2021-03-30 18:47:59 +0800

    camera, tampil foto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../service/foto.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public fotoService:FotoService) {}
+  tambahFoto(){
+    this.fotoService.tambahFoto()    
+  }
 
+  async ngOnInit(){
+    await this.fotoService.loadFoto()
+  }
 }
